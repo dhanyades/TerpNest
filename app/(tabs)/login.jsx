70f8ea86ef02f8eff782ab0563bcd4220815d38a
sarinali888 <sarinali888@gmail.com>
2025-01-21 00:42:10 -0800

    Update styling for login and survey pages

---
+++ app/(tabs)/login.jsx
@@ -0,0 +1,166 @@
+import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import terpLogo from '@/assets/images/turtlenestlogo.png'
+import umdLogo from '@/assets/images/umd-bckgrnd.webp'
+import userIcon from '@/assets/images/usericon.png'
+
+const LoginScreen = ({navigation}) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+  const correctUsername = "admin";
+  const correctPassword = "terps";
+
+  const handlePress = () => {
+    if (username === correctUsername && password === correctPassword){
+      navigation.navigate('Survey')
+
+    }
+    else{
+      window.alert("Try again!");
+    }
+  };
+    
+    return (
+        <View style={styles.main}>
+            <View style={styles.header}>
+                <Image style={styles.image} source={terpLogo} /> 
+                <Text style={styles.headerTitle}>TerpNest</Text>
+            </View>
+            <View style={styles.background}>
+                <Image style={styles.imagebckgrnd} source={umdLogo} />
+            </View>
+            <Text style={styles.label}>Log In:</Text>
+            <Text style={styles.subtitle}>Enter your username and password.</Text>
+            <View style={styles.profile}>
+              <Image style={{width:100, height:100}} source={userIcon} />
+            </View>
+            <View style={styles.login}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter your username"
+                    value={username}
+                    onChangeText={setUsername}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter your password"
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry={true}
+                />
+    
+                <TouchableOpacity  onPress={handlePress} style={styles.button}>
+                    <Text>Submit!</Text>
+                </TouchableOpacity>
+
+            </View>
+      </View>
+    );
+
+    
+}
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+    main: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: '#800020',
+      resizeMode: 'contain'
+    },
+    header: {
+        backgroundColor: '#C41E3A',
+        maxHeight: '9%',
+        maxWidth: '100%',
+        flexDirection: 'row',
+    },
+      title: {
+        fontSize: 64,
+        fontWeight: "bold",
+      },
+      subtitle: {
+        fontSize: 36,
+        color: "#fff",
+      },
+      profile:{
+        maxWidth: 100,
+        maxHeight: 100,
+        alignSelf: 'center'
+      },
+      input: {
+        width: "80%",
+        height: 40,
+        marginVertical: 10,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 5,
+        backgroundColor: "#fff",
+      },
+      headerTitle: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        fontFamily: 'Monaco',       
+        color: 'white',
+        opacity: 0.8,
+        alignSelf: 'center', 
+        marginLeft: '-5%',
+    },
+    image: {
+        maxHeight: '100%',
+        maxWidth: '24%',
+        resizeMode: 'contain'
+    },
+    background: {
+        maxHeight: '30%',
+        backgroundColor: 'black',
+        maxWidth: '100%',
+    },
+    imagebckgrnd: {
+        maxHeight: '100%',
+        maxWidth: '100%',
+        opacity: 0.5,
+    },
+    label: {
+        fontSize: 60,
+        fontWeight: "bold",
+        fontFamily: 'Lucida Console',
+        opacity: 0.8,
+        marginLeft: 20,
+      
+    },
+    subtitle: {
+        fontSize: 18,
+        color: "#fff",
+        opacity: 0.7,
+        marginLeft: 23,
+        marginBottom: 20
+    },
+    login: {
+        alignItems: 'center',
+    },
+    input: {
+        width: "70%",
+        height: 40,
+        marginVertical: 10,
+        borderWidth: 4,
+        padding: 15,
+        borderRadius: 10,
+        backgroundColor: "#fff",
+    },
+    button: {
+        padding: 10,
+        backgroundColor: 'white',
+        marginTop: '10%',
+        borderRadius: 20,
+        height: 40,
+        width: 90,
+        alignItems: 'center',
+        fontWeight: 'bold',
+    },
+    header: {
+        backgroundColor: '#C41E3A',
+        maxHeight: '9%',
+        maxWidth: '100%',
+        flexDirection: 'row',
+      },
+    });
---
+++ app/(tabs)/survey.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import {View, Text, TouchableOpacity, StyleSheet, Image} from "react-native";
+import terpLogo from '@/assets/images/turtlenestlogo.png'
+
+const SurveyScreen = ({navigation, username: string}) => {
+ return(
+    <View>
+        <View style={styles.header}>
+                <Image style={styles.image} source={terpLogo} /> 
+                <Text style={styles.title}>TerpNest</Text>
+        </View>
+        <View>
+            <Text style={styles.label}>Survey</Text>
+            <Text style={styles.sublabel}>Please fill in your preferences below:</Text>
+            <View style={styles.line}></View>
+            <Text style={styles.subtitle}>In terms of cleanliness, people consider me to be:</Text>
+            <Text style={styles.subtitle}>I am a morning person.</Text>
+            <Text style={styles.subtitle}>It would bother me to live with someone who smokes/vapes.</Text>
+            <Text style={styles.subtitle}>Socially, I am best described as:</Text>
+            <Text style={styles.subtitle}>Any dietary restrictions or allergies you wish to disclose:</Text>
+        </View>
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Submit Changes</Text>
+        </TouchableOpacity>
+    </View>
+ )   
+}
+export default SurveyScreen;
+
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: '#C41E3A',
+        maxHeight: '4.75%',
+        maxWidth: '100%',
+        flexDirection: 'row',
+    },
+    image: {
+        maxHeight: '100%',
+        maxWidth: '24%',
+        resizeMode: 'contain'
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        fontFamily: 'Monaco',       
+        color: 'white',
+        opacity: 0.8,
+        alignSelf: 'center', 
+        marginLeft: '-5%',
+    },
+    line: {
+        height: 1,
+        backgroundColor: 'black',
+        marginBottom: 20,
+    },
+    label: {
+        fontSize: 45,
+        fontWeight: 'bold',
+        fontFamily: 'Lucida Console',
+        marginLeft: 20, 
+    },
+    sublabel: {
+        fontSize: 22,
+        fontWeight: 'medium',
+        fontFamily: 'Lucida Console',
+        marginLeft: 20,
+        marginBottom: 5, 
+    },
+    subtitle: {
+        fontSize: 20,
+        fontFamily: 'Lucida Console',
+        marginLeft: 20,
+        marginRight: 20,
+        marginBottom: 60,
+    },
+    button: {
+        padding: 10,
+        backgroundColor: 'gray',
+        marginTop: '10%',
+        marginBottom: '10%',
+        borderRadius: 20,
+        height: 40,
+        width: 140,
+        alignSelf: 'center',
+    },
+    buttonText: {
+        fontSize: 15,
+        alignSelf: 'center',
+    },
+});
