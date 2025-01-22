64428387a992f52e59d4309fe85f0821d0528fbb
sarinali888 <sarinali888@gmail.com>
2025-01-21 18:27:06 -0800

    edit listing_scrape.py to get image url

--- components/Listing.tsx
+++ components/Listing.tsx
@@ -1,11 +1,13 @@
 class Card {
     title: string;  
+    image: string;
     address: string;
     price: string;
     beds: string
 
-    constructor(title: string, address: string, price: string, beds: string) {
+    constructor(title: string, image: string, address: string, price: string, beds: string) {
         this.title = title;
+        this.image = image;
         this.address = address;
         this.price = price;
         this.beds = beds;
